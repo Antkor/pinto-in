033363d8f7a5d8b39e63f5cd147f892979f39846
Antkor <antkorsc@gmail.com>
2021-10-27 08:28:25 +0300

    added login page styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { MenuController } from '@ionic/angular';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -12,10 +13,11 @@ export class AppComponent {
     { title: 'News', url: '/news', icon: 'newspaper' },
     { title: 'About', url: '/about', icon: 'information-circle' },
   ];
-  constructor(private router: Router) {}
+  constructor(private router: Router, private menu: MenuController) {}
 
   logout() {
     localStorage.removeItem('user');
+    this.menu.close();
     this.router.navigateByUrl('login');
   }
 }
